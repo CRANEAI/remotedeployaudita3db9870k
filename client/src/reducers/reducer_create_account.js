ea4ac6c3c613a324bbfe9486ed0503cd46cbd03a
Elly <elly@mg.crane.ai>
2018-11-02 11:36:28 -0400

    added client/src/reducers/reducer_create_account refactoring

---
+++ client/src/reducers/reducer_create_account.js
@@ -0,0 +1,11 @@
+var DEFAULT_STATE = {
+  redirect: false,
+  errorMessage: null
+}
+
+export default function(state = DEFAULT_STATE, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
