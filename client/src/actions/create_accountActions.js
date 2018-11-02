1e082fc2e91671d6a7f2507409da045e5df2bed1
Elly <elly@mg.crane.ai>
2018-11-02 11:40:31 -0400

    added client/src/actions/create_accountActions refactoring

---
+++ client/src/actions/create_accountActions.js
@@ -0,0 +1,5 @@
+/*
+                This will control the networking for the create account network requests
+            */
+import axios from "axios"
+import { client, url, setToken } from "../utils/axios.config.js"
