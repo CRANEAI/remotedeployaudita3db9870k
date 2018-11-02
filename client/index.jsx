bc11c8f1f5415698c99508bfb1f45fb487055f5f
Elly <elly@mg.crane.ai>
2018-11-02 11:36:41 -0400

    added client/index refactoring

---
+++ client/index.jsx
@@ -0,0 +1,47 @@
+
+        //----------------------------------------------------------------------------------------------------------
+        //Declariations and libraries
+        //----------------------------------------------------------------------------------------------------------
+        import React                                            from 'react';
+        import ReactDOM                                         from 'react-dom';
+        import { BrowserRouter as Router, Switch, Route }       from 'react-router-dom';
+        import { Provider }                                     from 'react-redux';
+        import { createStore, applyMiddleware }                 from 'redux';
+        import promise                                          from 'redux-promise';
+        import createMemoryHistory                              from 'history/createMemoryHistory';
+        import App                                              from './container/app.jsx';
+       
+        //----------------------------------------------------------------------------------------------------------
+            //Parent Components
+        //----------------------------------------------------------------------------------------------------------
+        
+        import create_account from './components/create account.jsx'
+       
+        //----------------------------------------------------------------------------------------------------------
+        //Reducer
+        //----------------------------------------------------------------------------------------------------------
+        import store                                            from './reducers/index.jsx';
+
+        //----------------------------------------------------------------------------------------------------------
+            //Middleware setup
+        //----------------------------------------------------------------------------------------------------------
+        const history                                 = createMemoryHistory();
+        const createStoreWithMiddleware               = applyMiddleware(promise)(createStore);
+
+        //----------------------------------------------------------------------------------------------------------
+        //Routes Setup
+        //----------------------------------------------------------------------------------------------------------
+        ReactDOM.render(
+            <Provider store={createStoreWithMiddleware(store, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+        
+                <Router basename="/" history={ history } >
+                    <Switch>
+                        <Route path "/" component={create_account}
+                        <Route path="/create_account" component={create_account} /> 
+
+                    </Switch>
+                </Router>
+            </Provider>,
+            document.getElementById('app') || document.createElement('div')
+          );
+        
